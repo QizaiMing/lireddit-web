e3406652f26765bde33253ad53ef550036c3642a
Juan Chacon <mahnimex@gmail.com>
2021-01-18 12:17:30 -0400

    Added ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { withUrqlClient } from 'next-urql'
 import { Navbar } from '../components/Navbar'
+import { usePostsQuery } from '../generated/graphql'
+import { createUrqlClient } from '../utils/createUrqlClient'
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>hello world</div>
-  </>
-)
+const Index = () => {
+  const [{ data }] = usePostsQuery()
 
-export default Index
+  return (
+    <>
+      <Navbar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((post) => {
+          return <div key={post.id}>{post.title}</div>
+        })
+      )}
+    </>
+  )
+}
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index)
